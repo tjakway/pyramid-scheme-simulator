db2fb450395157b2c3bcba7eceb34e553b3a2e75
Thomas Jakway <tjakway@nyu.edu>
2017-11-07 20:52:37 -0500

    added graph typedefs

--- include/Graph.hpp
+++ include/Graph.hpp
@@ -2,7 +2,22 @@
 
 #include <vector>
 
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+
+#include "CapitalHolder.hpp"
+
 namespace pyramid_scheme_simulator {
+
+using PopulationGraph = 
+    boost::adjacency_list<
+        boost::vecS,
+        boost::vecS, 
+        boost::undirectedS,
+        CapitalHolder>;
+
+using PopDescriptor = 
+    boost::graph_traits<PopulationGraph>::vertex_descriptor;
 
 enum TransformationType
 {
@@ -13,12 +28,14 @@ enum TransformationType
 class Transformation
 {
     //XXX
-    //virtual void transform(graph, )
+    virtual void transform(PopulationGraph* graph, 
+            PopDescriptor to,
+            PopDescriptor from) = 0;
     virtual TransformationType getTransformationType() = 0;
 };
 
 //a step in the simulator is a set of transformations
 //XXX: should each step also return a graph?
-using Step = std::vector<Transformation>
+using Step = std::vector<Transformation>;
 
 } //pyramid_scheme_simulator
