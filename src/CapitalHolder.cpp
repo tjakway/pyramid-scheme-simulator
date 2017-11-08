8c7f8ad04f5acaf8b2deb88c4078e257df41a653
Thomas Jakway <tjakway@nyu.edu>
2017-11-07 21:28:34 -0500

    now using getSalesChance as a function passed to Distributor

--- src/CapitalHolder.cpp
+++ src/CapitalHolder.cpp
@@ -2,14 +2,4 @@
 
 namespace pyramid_scheme_simulator {
 
-/**
- * obviously the chance of making a sale to the company is 0
- * as is the chance of selling to another distributor
- */
-double Distributor::getSalesChance(const CapitalHolder& other)
-{
-    //TODO
-    return -1;
-}
-
 }
