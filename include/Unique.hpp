8e35065fe8192071e1746757bf2203561c0dfbb8
Thomas Jakway <tjakway@nyu.edu>
2017-11-07 20:33:11 -0500

    made Unique a thin layer over crossguid

--- include/Unique.hpp
+++ include/Unique.hpp
@@ -1,47 +1,22 @@
 #pragma once
 
+#include "Guid.hpp"
 #include "RDType.hpp"
 
 namespace pyramid_scheme_simulator {
 
-class Unique;
 class Uniqueable;
 
-class UniqueGen 
-{
-private:
-    const rd_ptr rd;
-
-public:
-    UniqueGen(rd_ptr p) : rd(p) {}
-    Unique newUnique();
-};
+using Unique = xg::Guid;
 
 /**
- * a class that represents and generates unique IDs
- */
-class Unique 
-{
-private:
-    friend UniqueGen;
-
-    rd_type id;
-    Unique(rd_type x) : id(x) {}
-
-public:
-    bool operator==(const Unique& rhs){ this->id == rhs.id; }
-    bool operator!=(const Unique& rhs){ return !(*this == rhs); }
-};
-
-
-/**
- * TODO: implement
  * a trait for classes with Unique IDs
- * TODO: ought to be pure virtual
  */
 class Uniqueable 
 {
-    Unique id;
+public:
+    const Unique id;
+    Uniqueable::Uniqueable() : id(xg::newGuid()) {}
 };
 
 }
