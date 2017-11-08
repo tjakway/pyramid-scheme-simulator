27f32722bbe893e0d92f142f057b500f4602278f
Thomas Jakway <tjakway@nyu.edu>
2017-11-07 21:41:45 -0500

    mkRandomSalesFunction now checks that the purchaser has enough money

---
+++ include/ParameterImplementations.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <functional>
+#include <random>
+
+#include "RDType.hpp"
+#include "CapitalHolder.hpp"
+
+
+namespace pyramid_scheme_simulator {
+
+/**
+ * return a sales function that is totally random and not dependent 
+ */
+Distributor::SalesFunction mkRandomSalesFunction(const unsigned int productCost,
+        rd_ptr randomGen) {
+    return [=](const CapitalHolder const& potentialBuyer) -> double {
+        if(potentialBuyer.canPurchase(productCost)) {
+            return std::uniform_real_distribution()(randomGen);
+        }
+        //can't buy it without money
+        else {
+            return 0;
+        }
+    };
+}
+
+}
