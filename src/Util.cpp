1aa94bc63d870ed13a4fc1bda5b87ef9ef4d8e05
Thomas Jakway <tjakway@nyu.edu>
2017-11-13 14:43:35 -0500

    added explicit template parameter

--- src/Util.cpp
+++ src/Util.cpp
@@ -21,7 +21,7 @@ bool Util::sampleFrom(rd_ptr rng, double probability)
         //XXX: maybe overcomplicating this
         //sample on the interval [0, 1] instead of [0, 1)
 
-        static auto dist = std::uniform_real_distribution(0.0, 
+        static auto dist = std::uniform_real_distribution<double>(0.0, 
                 std::nextafter(1.0, std::numeric_limits<double>::max()));
 
         return dist(rng) <= probability;
