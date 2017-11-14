1a5380ab443f6573537f5f39b6eefee8d4e09890
Thomas Jakway <tjakway@nyu.edu>
2017-11-14 14:45:27 -0500

    fixed const madness in toIndicesPair

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -5,6 +5,9 @@
 #include <set>
 #include <array>
 #include <utility>
+#include <memory>
+#include <iterator>
+#include <exception>
 
 namespace pyramid_scheme_simulator {
 
@@ -13,9 +16,9 @@ PopulationGraph* PopulationGraphGenerator::
 {
     struct UndirectedEdge
     {
-        Unique v1, v2;
+        const Unique v1, v2;
 
-        bool operator==(const Edge& other) const
+        bool operator==(const UndirectedEdge& other) const
         {
             //since it's undirected it's equivalent
             //if the vertices are swapped
@@ -23,23 +26,48 @@ PopulationGraph* PopulationGraphGenerator::
                 (v1 == other.v2 && v2 == other.v1);
         }
 
-        bool operator!=(const Edge& other) const
+        bool operator!=(const UndirectedEdge& other) const
         {
             return !this->operator==(other);
         }
 
-        std::pair<int, int> toIndicesPair(std::array<Unique> vertices) const {
-            auto firstElem = std::find(vertices.begin(), vertices.end(), v1);
-            auto secondElem = std::find(vertices.begin(), vertices.end(), v2);
-            return std::make_pair(*firstElem, *secondElem);
+        std::pair<int, int> toIndicesPair(Unique* vertices, int length) const {
+            //sanity check--should never happen
+            class VertexNotFoundException : public std::exception {
+                const Unique* which;
+            public:
+                VertexNotFoundException(const Unique* which) : which(which) {}
+
+                const char* what() const throw() override {
+                    std::string msg = "Could not find the index of vertex " 
+                        + std::string(*which);
+                    return msg.c_str();
+                }
+            };
+
+            const Unique* begin = vertices;
+            const Unique* end = begin + length;
+
+            auto checkFind = [=](const Unique* which, const Unique* res) { 
+                if(res == end) {
+                    throw VertexNotFoundException(which);
+                }
+            };
+            
+            auto firstElem = std::find(begin, end, v1);
+            checkFind(&v1, firstElem);
+            auto secondElem = std::find(begin, end, v2);
+            checkFind(&v2, secondElem);
+
+            return std::make_pair(begin - firstElem, begin - secondElem);
         }
     };
 
     //check if link chance procs
-    auto testEdge = [rd]() { return Util::sampleFrom(rd, options.linkChance); }
+    auto testEdge = [rd]() -> bool { return Util::sampleFrom(rd, options.linkChance); }
 
     std::set<UndirectedEdge> edges;
-    std::array<Unique> vertices;
+    std::unique_ptr<Unique> vertices;
     
     //generate vertices with random ids
     for(unsigned long i = 0; i < options.graphSize; i++)
