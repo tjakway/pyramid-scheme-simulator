37009058bb220a8465cf01b9e4e694fb1a6d6ac7
Thomas Jakway <tjakway@nyu.edu>
2017-11-14 13:36:35 -0500

    skeleton code

---
+++ src/Config.cpp
@@ -0,0 +1,11 @@
+#include "Config.hpp"
+
+namespace pyramid_scheme_simulator {
+
+void Config::GraphGenerationOptions::
+    checkSetMaxEdgesPerNode(unsigned int maxEdgesPerNode, bool allowUnconnectedSubgraphs)
+{
+
+}
+
+}
