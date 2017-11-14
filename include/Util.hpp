16890e1ef866fee2da78ebae132dbcae3b0a0346
Thomas Jakway <tjakway@nyu.edu>
2017-11-14 10:10:09 -0500

    added selectRandomly from https://stackoverflow.com/questions/6942273/get-random-element-from-container

--- include/Util.hpp
+++ include/Util.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <random>
+#include <iterator>
+
 #include "Types.hpp"
 
 namespace pyramid_scheme_simulator {
@@ -16,6 +19,16 @@ public:
      * chance that bit is 1
      */
     static bool sampleFrom(rd_ptr, double);
+
+    /**
+     * from https://stackoverflow.com/questions/6942273/get-random-element-from-container
+     */
+    template<typename Iter, typename RandomGenerator>
+    static Iter selectRandomly(Iter start, Iter end, RandomGenerator& g) {
+        std::uniform_int_distribution<> dis(0, std::distance(start, end) - 1);
+        std::advance(start, dis(g));
+        return start;
+    }
 };
 
 }
