6adf32f82f90f1d5a269ae2c8b6f14fd1d930d3e
Thomas Jakway <tjakway@nyu.edu>
2017-11-16 10:14:56 -0500

    wrote deductMoney (throws exception on insufficient funds)

--- src/CapitalHolder.cpp
+++ src/CapitalHolder.cpp
@@ -1,6 +1,21 @@
 #include "CapitalHolder.hpp"
 
+#include <string>
+#include <sstring>
+#include <exception>
+
 namespace pyramid_scheme_simulator {
+
+void CapitalHolder::setMoney(Money m)
+{
+    money = m;
+}
+
+Money CapitalHolder::getMoney()
+{
+    return money;
+}
+
 
 /**
  * compare unique IDs
@@ -15,6 +30,33 @@ bool CapitalHolder::operator!=(const CapitalHolder& other)
     return this->id != other.id;
 }
 
+void Consumer::deductMoney(Money howMuch)
+{
+    //should never happen
+    class InsufficientFundsException : public std::exception
+    {
+        std::string msg;
+    public:
+        InsufficientFundsException(std::string m) : msg(m) {}
+        const char* what() const throw() override {
+            return msg.c_str();
+        }
+    };
+
+    if(getMoney() <  howMuch)
+    {
+        std::stringstream ss;
+        ss << "Cannot deduct " << howMuch <<
+            " from account with only " << getMoney() <<
+            std::endl;
+        throw InsufficientFundsException(ss.str());
+    }
+    else
+    {
+        setMoney(getMoney() - howMuch);
+    }
+}
+
 bool Distributor::canPurchase(Money cost, const CapitalHolder& from)
 {
     if(*this != from)
@@ -25,6 +67,11 @@ bool Distributor::canPurchase(Money cost, const CapitalHolder& from)
     {
         //XXX
     }
+}
+
+void Distributor::addMoney(Money howMuch)
+{
+    setMoney(getMoney() + howMuch);
 }
 
 }
