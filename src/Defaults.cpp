e738f7545e129e1b0eb60cc0415c276517665b4f
Thomas Jakway <tjakway@nyu.edu>
2017-11-17 09:16:12 -0500

    finished implementing mkConsumer, directly passing the generated chance distributions

--- src/Defaults.cpp
+++ src/Defaults.cpp
@@ -1,4 +1,4 @@
-#include "Defaults.hpp"
+#include "Config.hpp"
 #include "Unique.hpp"
 #include "Types.hpp"
 #include "Actors.hpp"
@@ -7,16 +7,19 @@
 
 namespace pyramid_scheme_simulator {
 
-const double Defaults::defaultSalesChance = 0.1;
-const double Defaults::defaultConversionChance = 0.1;
+Config::Defaults::Defaults(Config* config) : config(config)
+    {}
 
-const std::function<std::shared_ptr<Consumer>(Unique, Money)> 
-    Defaults::mkDefaultConsumer = 
-        [](Unique u, Money m){
-            return std::make_shared<StaticConsumer>(
-                    u, m,
-                    defaultSalesChance,
-                    defaultConversionChance
-                    );
-        };
+
+std::shared_ptr<Consumer> Config::Defaults::mkConsumer(rd_ptr rd, Unique u)
+{
+    const auto startingMoney = config->simulationOptions->startingFunds();
+    //since we're making a StaticConsumer we only need 1 
+    const auto salesChance = config->salesSkillDistribution(rd);
+    const auto conversionChance = config->conversionChanceDistribution(rd);
+
+    return std::make_shared<StaticConsumer>(u, startingMoney,
+            salesChance, conversionChance);
+}
+
 }
