d40c503a118c67187665757c7895293c4f21814c
Thomas Jakway <tjakway@nyu.edu>
2017-11-18 11:17:38 -0500

    fixup! added mappend_move to Monoid

--- include/Util/Monoid.hpp
+++ include/Util/Monoid.hpp
@@ -12,6 +12,10 @@ class Monoid
 
     T mappend(T& other);
 
+    /**
+     * can be implemented as a performance optimization in cases where
+     * the original could be moved instead of copied
+     */
     T mappend_move(T& other) {
         return mappend(other);
     }
