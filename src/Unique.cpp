27c85f53369127bfaa87577b39ca4eba7d7eac87
Thomas Jakway <tjakway@nyu.edu>
2017-11-20 18:15:49 -0500

    fixed emptyUniqueSet

--- src/Unique.cpp
+++ src/Unique.cpp
@@ -7,6 +7,6 @@ namespace pyramid_scheme_simulator {
 
 static const Unique emptyUnique = Unique(xg::Guid());
 
-const UniqueSet& UniqueSet::emptyUniqueSet = &Unique::emptyUnique;
+const UniqueSet& UniqueSet::emptyUniqueSet = Unique::emptyUnique;
 
 }
