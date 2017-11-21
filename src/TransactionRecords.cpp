37d2e593103ac112dab28e24b50abd37f4b8b3da
Thomas Jakway <tjakway@nyu.edu>
2017-11-21 15:23:49 -0500

    rewrote compareUniqueables in terms of compareUniques

--- src/TransactionRecords.cpp
+++ src/TransactionRecords.cpp
@@ -9,7 +9,26 @@ namespace pyramid_scheme_simulator {
 
 
 bool compareUniqueables(Uniqueable* fst,
-        Uniqueable* snd)
+        Uniqueable* snd) {
+    const bool fstEmpty = fst == nullptr;
+    const bool sndEmpty = snd == nullptr;
+
+    //check for null pointers
+    if(fstEmpty && sndEmpty) {
+        return false;
+    }
+    else if(!fstEmpty && sndEmpty) {
+        return false;
+    }
+    else if(fstEmpty && !sndEmpty) {
+        return true;
+    } else {
+
+        return compareUniques(&fst->id, &snd->id);
+    }
+}
+ 
+bool compareUniques(Unique* fst, Unique* snd)
 {
     const bool fstEmpty = fst == nullptr;
     const bool sndEmpty = snd == nullptr;
