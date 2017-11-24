41c98bb9757a15181254f3ae9783bfe0c0e03159
Thomas Jakway <tjakway@nyu.edu>
2017-11-23 22:45:04 -0500

    fixed static field initialization

--- src/Unique.cpp
+++ src/Unique.cpp
@@ -3,7 +3,7 @@
 
 namespace pyramid_scheme_simulator {
 
-static const Unique emptyUnique = Unique(xg::Guid());
+const Unique Unique::emptyUnique = Unique(xg::Guid());
 
 const UniqueSet& UniqueSet::emptyUniqueSet = Unique::emptyUnique;
 
