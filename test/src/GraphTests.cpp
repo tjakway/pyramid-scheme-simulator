3b83114341365aeadac24195101a17bec361f25e
Thomas Jakway <tjakway@nyu.edu>
2017-11-25 16:16:07 -0500

    added BasicRestockTest, reorganized GraphTests into fixture

--- test/src/GraphTests.cpp
+++ test/src/GraphTests.cpp
@@ -17,25 +17,27 @@
 
 namespace pyramid_scheme_simulator {
 
-static Money startingMoney = 100;
 
-static PopulationGraph::Pop distributor = 
-    std::make_shared<EagerTestDistributor>(startingMoney);
-
-static PopulationGraph::Pop customer1 = 
-    std::make_shared<EagerTestConsumer>(startingMoney);
-
-static PopulationGraph::Pop customer2 = 
-    std::make_shared<EagerTestConsumer>(startingMoney);
-
-static std::vector<std::pair<PopulationGraph::Pop, PopulationGraph::Pop>>
-    tinyGraphTuples = {
-            std::make_pair(distributor, customer1),
-            std::make_pair(distributor, customer2),
-            std::make_pair(customer1, customer2) };
-
-TEST(GraphTests, BasicTest)
+class BasicGraphTests : public ::testing::Test
 {
+public:
+    Money startingMoney = 100;
+
+    PopulationGraph::Pop distributor = 
+        std::make_shared<EagerTestDistributor>(startingMoney);
+
+    PopulationGraph::Pop customer1 = 
+        std::make_shared<EagerTestConsumer>(startingMoney);
+
+    PopulationGraph::Pop customer2 = 
+        std::make_shared<EagerTestConsumer>(startingMoney);
+
+    std::vector<std::pair<PopulationGraph::Pop, PopulationGraph::Pop>>
+        tinyGraphTuples = {
+                std::make_pair(distributor, customer1),
+                std::make_pair(distributor, customer2),
+                std::make_pair(customer1, customer2) };
+
     std::unique_ptr<PopulationGraph> tinyGraph = 
         make_unique<MockPopulationGraph>(tinyGraphTuples);
 
@@ -44,12 +46,27 @@ TEST(GraphTests, BasicTest)
 
     rd_ptr rd = std::make_shared<std::mt19937_64>();
 
+
     const Money price = 10;
     const SimulationTick when = 0;
+};
 
-    auto recordResult = (*saleHandler)(when, price, rd, *distributor, *customer1);
+TEST_F(BasicGraphTests, BasicTest)
+{
+    auto recordResult = (*saleHandler)(when, price, 
+            rd, *distributor, *customer1);
 
     ASSERT_TRUE(recordResult.records.size() > 0);
+}
+
+TEST_F(BasicGraphTests, BasicRestockTest)
+{
+    //sale shouldn't happen if there's no inventory
+    distributor->setInventory(0);
+    auto recordResult = (*saleHandler)(when, price, 
+            rd, *distributor, *customer1);
+
+    ASSERT_EQ(recordResult.records.size(), 0);
 }
 
 }
