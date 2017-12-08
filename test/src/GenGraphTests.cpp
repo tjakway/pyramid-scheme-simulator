a07443a13249daa841fb3f2f4b9c25cd8248e158
Thomas Jakway <tjakway@nyu.edu>
2017-12-08 15:38:22 -0500

    build generation still works

---
+++ test/src/GenGraphTests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include "Config.hpp"
+#include "Util/Util.hpp"
+#include "Util/Unique.hpp"
+#include "TransactionRecords.hpp"
+#include "TransactionObjects.hpp"
+#include "PopulationGraph.hpp"
+#include "Types.hpp"
+#include "CapitalHolderClassDecls.hpp"
+#include "CapitalHolder.hpp"
+#include "PopulationGraph.hpp"
