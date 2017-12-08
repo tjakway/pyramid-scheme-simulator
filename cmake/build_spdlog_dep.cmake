f73ee5b063262d3c6347950b4b06afb403ebeacc
Thomas Jakway <tjakway@nyu.edu>
2017-12-08 15:32:18 -0500

    first iteration trying to include spdlog in the build

---
+++ cmake/build_spdlog_dep.cmake
@@ -0,0 +1,7 @@
+#configure spdlog as a dependency
+set(SPDLOG_BUILD_TESTING OFF)
+
+set(SPDLOG_DIR ${CMAKE_SOURCE_DIR}/lib/spdlog)
+add_subdirectory(${SPDLOG_DIR} ${CMAKE_BINARY_DIR}/spdlog-bin)
+
+include_directories(${SPDLOG_DIR}/include SYSTEM)
