f76710f76aa694e77c0c3ca1efd41393427407d0
Thomas Jakway <tjakway@nyu.edu>
2017-12-11 09:11:20 -0800

    wrote assetInRangeInclusive

--- test/include/TestUtil.hpp
+++ test/include/TestUtil.hpp
@@ -4,6 +4,16 @@
 
 #include <gtest/gtest.h>
 #include <string>
+#include <utility>
+
+namespace {
+    //ASSERT/EXPECT_PRED need a function whose return type is bool
+    template <typename T>
+    bool isInRange(T value, T le, T ge)
+    {
+        return le <= value && value <= ge;
+    }
+}
 
 namespace pyramid_scheme_simulator {
 
@@ -19,6 +29,12 @@ R assertRight(Either<L, R> either, std::string msg = std::string())
 {
     ASSERT_TRUE(either.isRight()) << msg;
     return either.getRight();
+}
+
+template <typename T>
+::testing::AssertionResult assertBetweenInclusive(T value, std::pair<T, T> range)
+{
+    return ASSERT_PRED3(isInRange, value, range.first, range.second);
 }
 
 }
