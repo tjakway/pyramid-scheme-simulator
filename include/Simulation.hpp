b5996096c927556d4c3ce8885710e101b58db15e
Thomas Jakway <tjakway@nyu.edu>
2017-12-14 21:15:22 -0800

    wrote lookupConversionRecord

--- include/Simulation.hpp
+++ include/Simulation.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <memory>
+#include <algorithm>
 
 #include "Config.hpp"
 #include "PopulationGraph.hpp"
+#include "TransactionObjects.hpp"
+#include "TransactionRecords.hpp"
 
 namespace pyramid_scheme_simulator {
 
@@ -15,6 +18,25 @@ private:
 
     std::unique_ptr<PopulationGraph> buildGraph(std::shared_ptr<Config>);
     void tick();
+    
+protected:
+    ConversionHandler::Conversion* lookupConversionRecord(ConversionHandler::RecordType recs,
+            Unique possibleConvertId)
+    {
+        auto res = std::find_if(recs.records.begin(), recs.records.end(),
+                [=](const PopulationGraph::Pop x){
+                    return x->id == possibleConvertId;
+                });
+
+        if(res == recs.records.end())
+        {
+            return nullptr;
+        }
+        else
+        {
+            return (*res).get();
+        }
+    }
 
 public:
     Simulation(Config*);
