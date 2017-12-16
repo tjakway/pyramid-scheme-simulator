797e6c1b28fccd9d4d71902004141f6ed08071ea
Thomas Jakway <tjakway@nyu.edu>
2017-12-16 12:45:20 -0800

    finished testMapCollection

--- test/src/UtilTests.cpp
+++ test/src/UtilTests.cpp
@@ -1,7 +1,12 @@
 #include <gtest/gtest.h>
 
-#include <algorithm>
 #include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <deque>
+#include <list>
 
 #include "Util/Util.hpp"
 
@@ -14,7 +19,7 @@ namespace {
         const int generatedSize = (rand() % 100) + 1;
         for(int i = 0; i < generatedSize; i++)
         {
-            from.insert(rand());
+            from.insert(from.end(), rand());
         }
 
         EXPECT_EQ(from.size(), generatedSize);
@@ -22,7 +27,7 @@ namespace {
         //some random mapping function
         std::function<typename ToCollection::value_type(typename FromCollection::value_type)>
             mapping = 
-            [](typename FromCollection::value_type x) -> typename ToCollection::value_Type
+            [](typename FromCollection::value_type x) -> typename ToCollection::value_type
             {
                 return x + 1;
             };
@@ -32,6 +37,29 @@ namespace {
                                                         ToCollection>(from, mapping);
 
         EXPECT_EQ(to.size(), generatedSize);
-        ASSERT_TRUE(std::equal(from.begin(), from.end(), to.begin(), to.end()));
+
+        typename FromCollection::iterator fIt = from.begin(),
+                 fEnd = from.end();
+        typename ToCollection::iterator tIt = to.begin(),
+                 tEnd = to.end();
+
+        while(fIt != fEnd)
+        {
+            //the mapping added 1
+            ASSERT_EQ((*fIt) + 1, *tIt);
+            ++tIt;
+            ++fIt;
+        }
+
+        ASSERT_EQ(tIt, tEnd);
     }
+}
+
+namespace pyramid_scheme_simulator {
+
+TEST(UtilTests, testMapCollectionVectorToVector)
+{
+    testMapCollection<std::vector<int>, std::vector<int>>();
+}
+
 }
