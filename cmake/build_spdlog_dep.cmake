806e4ba86bcef7152870afa1c887a913b539481e
Thomas Jakway <tjakway@nyu.edu>
2017-12-18 18:57:04 -0800

    don't build spdlog tests

--- cmake/build_spdlog_dep.cmake
+++ cmake/build_spdlog_dep.cmake
@@ -1,5 +1,5 @@
 #configure spdlog as a dependency
-set(SPDLOG_BUILD_TESTING OFF)
+set(SPDLOG_BUILD_TESTING CACHE FORCE OFF)
 
 set(SPDLOG_DIR ${CMAKE_SOURCE_DIR}/lib/spdlog)
 add_subdirectory(${SPDLOG_DIR} ${CMAKE_BINARY_DIR}/spdlog-bin)
