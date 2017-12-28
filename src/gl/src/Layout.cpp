30dffe48a619048a00d816c2344f55f141fe447c
Thomas Jakway <tjakway@nyu.edu>
2017-12-27 21:00:44 -0800

    wrote applyCoulombsLaw

--- src/gl/src/Layout.cpp
+++ src/gl/src/Layout.cpp
@@ -17,9 +17,38 @@ void GraphLayout::Layout::applyCoulombsLaw()
 
         for(; j != jEnd; ++j)
         {
-            if(graph[*i].point != graph[*j].point)
+            const Point firstPoint = graph[*i].getPoint(),
+                        secondPoint = graph[*j].getPoint();
+            
+            if(firstPoint != secondPoint)
             {
+                const Position d = 
+                    firstPoint.position.subtract(
+                            secondPoint.position);
 
+                //add 0.1 to "avoid massive forces at small distances (and divide by zero)"
+                const double distance = 
+                    d.magnitude() + 0.1;
+
+                const Vector direction = d.normalise();
+
+                const Vector repulse = 
+                        direction.multiply(repulsion);
+                const double distanceSqr = distance * distance;
+
+                //apply forces to each point and replace the
+                //points in the graph with the new positions
+                graph[*i].setPoint(
+                 firstPoint
+                    .applyForce(
+                        repulse
+                         .divide(distanceSqr * 0.5)));
+
+                graph[*j].setPoint(
+                 secondPoint
+                    .applyForce(
+                        repulse
+                         .divide(distanceSqr * (-0.5))));
             }
         }
     }
