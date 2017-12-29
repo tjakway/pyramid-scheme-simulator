254b09bb06ca5020c23485daab6baa76b8bdecd6
Thomas Jakway <tjakway@nyu.edu>
2017-12-28 16:53:21 -0800

    reorganized gl to fit the new source dir layout: no src/gl/src, just src/gl and moved src/gl/include to include/gl

---
+++ test/src/gl/TestGraphLayout.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+#include "BasicGraphSetup.hpp"
+
+namespace pyramid_scheme_simulator {
+
+using BasicGraphLayoutTests = BasicGraphSetup;
+
+TEST_F(BasicGraphLayoutTests, BasicTest)
+{
+
+}
+
+}
