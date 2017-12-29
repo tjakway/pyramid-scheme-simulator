664c32799170274020ce17d6a26fc81a241ae437
Thomas Jakway <tjakway@nyu.edu>
2017-12-28 17:01:26 -0800

    wrote first test

--- test/src/gl/TestGraphLayout.cpp
+++ test/src/gl/TestGraphLayout.cpp
@@ -1,14 +1,17 @@
 #include <gtest/gtest.h>
 
+#include "gl/GraphLayout.hpp"
+
 #include "BasicGraphSetup.hpp"
 
 namespace pyramid_scheme_simulator {
 
 using BasicGraphLayoutTests = BasicGraphSetup;
 
-TEST_F(BasicGraphLayoutTests, BasicTest)
+TEST_F(BasicGraphLayoutTests, TestCTOR)
 {
-
+    GraphLayout {Config::Defaults::defaultGraphLayoutOptions,
+        *tinyGraph };
 }
 
 }
