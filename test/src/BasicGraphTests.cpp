3cd6a4874c853f6f250ea88209039b6c4a5cac89
Thomas Jakway <tjakway@nyu.edu>
2017-12-28 16:46:00 -0800

    refactored the test harness from BasicGraphTests into BasicGraphSetup and used a typedef to keep the test names the same

--- test/src/BasicGraphTests.cpp
+++ test/src/BasicGraphTests.cpp
@@ -15,6 +15,8 @@
 #include "mocks/MockTransactionObjects.hpp"
 #include "mocks/EagerActors.hpp"
 
+#include "BasicGraphSetup.hpp"
+
 #include <unordered_map>
 #include <functional>
 #include <vector>
@@ -26,39 +28,7 @@
 
 namespace pyramid_scheme_simulator {
 
-
-class BasicGraphTests : public ::testing::Test
-{
-public:
-    Money startingMoney = 100;
-
-    PopulationGraph::Pop distributor = 
-        std::make_shared<EagerTestDistributor>(startingMoney);
-
-    PopulationGraph::Pop consumer1 = 
-        std::make_shared<EagerTestConsumer>(startingMoney);
-
-    PopulationGraph::Pop consumer2 = 
-        std::make_shared<EagerTestConsumer>(startingMoney);
-
-    std::vector<std::pair<PopulationGraph::Pop, PopulationGraph::Pop>>
-        tinyGraphTuples = {
-                std::make_pair(distributor, consumer1),
-                std::make_pair(distributor, consumer2),
-                std::make_pair(consumer1, consumer2) };
-
-    std::unique_ptr<PopulationGraph> tinyGraph = 
-        make_unique<MockPopulationGraph>(tinyGraphTuples);
-
-    std::unique_ptr<SaleHandler> saleHandler = 
-        make_unique<MockSaleHandler>();
-
-    rd_ptr rd = std::make_shared<std::mt19937_64>();
-
-    const Money price = 10;
-    const SimulationTick when = 0;
-};
-
+using BasicGraphTests = BasicGraphSetup;
 
 TEST_F(BasicGraphTests, BasicSaleTest)
 {
