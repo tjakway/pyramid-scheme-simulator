e6df5e8e44e4cf203fcee535abf03dda37cf20e4
Thomas Jakway <tjakway@nyu.edu>
2017-12-28 21:50:48 -0800

    added dependency on GLEW

--- include/gl/GLUtil.hpp
+++ include/gl/GLUtil.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <GL/glew.h>
 #include <GL/gl.h>
+
+#include <string>
+#include <functional>
 
 #include "Util/NewExceptionType.hpp"
 
@@ -8,8 +12,16 @@ class GLUtil
 {
     GLUtil() = delete;
 
-    static std::string getShaderInfoLog(GLuint);
     static void throwIfErrorInShader(GLuint);
+
+    
+    static std::string getShaderStringValue(
+        GLuint, 
+        GLenum, 
+        std::function<void(GLuint, GLsizei, GLsizei*, GLchar*)>);
+
+    static std::string getShaderInfoLog(GLuint);
+    static std::string getShaderSource(GLuint);
 
 public:
     NEW_EXCEPTION_TYPE(GLUtilException);
