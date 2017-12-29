920f1f4203de282e9c4d44e55389e6dc5aa7418c
Thomas Jakway <tjakway@nyu.edu>
2017-12-28 21:50:48 -0800

    added dependency on GLEW

--- src/gl/GLUtil.cpp
+++ src/gl/GLUtil.cpp
@@ -2,10 +2,28 @@
 
 #include "Util/Strcat.hpp"
 
+#include <functional>
 #include <memory>
 
+#include <GL/glew.h>
 #include <GL/gl.h>
 
+
+std::string GLUtil::getShaderInfoLog(GLuint shader)
+{
+    return GLUtil::getShaderStringValue(shader, GL_INFO_LOG_LENGTH, 
+            [](GLuint s, GLsizei m, GLsizei* l, GLchar* i) {
+                glGetShaderInfoLog(s, m, l, i);
+            });
+}
+
+std::string GLUtil::getShaderSource(GLuint shader)
+{
+    return GLUtil::getShaderStringValue(shader, GL_SHADER_SOURCE_LENGTH, 
+            [](GLuint s, GLsizei m, GLsizei* l, GLchar* i) {
+                glGetShaderSource(s, m, l, i);
+            });
+}
 
 void GLUtil::throwIfErrorInShader(GLuint shader)
 {
@@ -14,37 +32,43 @@ void GLUtil::throwIfErrorInShader(GLuint shader)
 
     if(status != GL_TRUE)
     {
-        throw GLUtilException(STRCAT("Error in shader "));
+        throw GLUtilException(STRCAT("Error in shader with source: ",
+                    std::endl, getShaderSource(shader),
+                    std::endl, "shader info log: ",
+                    getShaderInfoLog(shader), std::endl));
     }
 }
 
 /**
  * returns the info log for that shader or a blank string if there isn't one
  */
-std::string GLUtil::getShaderInfoLog(GLuint shader, )
+std::string GLUtil::getShaderStringValue(
+        GLuint shader, 
+        GLenum lengthEnum, 
+        std::function<void(GLuint, GLsizei, GLsizei*, GLchar*)> query)
 {
-    GLint logLength = -1;
-    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLength);
+    GLint lengthParam = -1;
+    glGetShaderiv(shader, lengthEnum, &lengthParam);
 
-    if(logLength > 0)
+    if(lengthParam > 0)
     {
-        std::unique_ptr<char> shaderInfoLog 
-            = std::unique_ptr<char>(new char[logLength]);
+        std::unique_ptr<char> buf 
+            = std::unique_ptr<char>(new char[lengthParam]);
 
         GLint writtenLength = -1;
-        glGetShaderInfoLog(shader, logLength, &writtenLength, shaderInfoLog.get());
+        query(shader, lengthParam, &writtenLength, buf.get());
 
-        std::string infoLog(shaderInfoLog.get());
+        std::string strValue(buf.get());
 
-        if(writtenLength != logLength)
+        if(writtenLength != lengthParam)
         {
             throw GLUtil::GLUtilException(STRCAT(
-                    "Got log length of ", writtenLength, " but expected ", logLength, " for "
-                    " info log: ", infoLog));
+                    "Got length of ", writtenLength, " but expected ", lengthParam, " for "
+                    " shader string characteristic: ", strValue));
         }
         else
         {
-            return infoLog;
+            return strValue;
         }
     }
     else
@@ -63,4 +87,5 @@ GLuint GLUtil::compileShaderProgram(
 
     }
 
+    return vs;
 }
