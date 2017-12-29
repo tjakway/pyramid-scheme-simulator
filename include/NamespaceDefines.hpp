74c239fced258445e237b54c19b44ed862d83968
Thomas Jakway <tjakway@nyu.edu>
2017-12-29 11:44:55 -0800

    using macros to begin and end namespaces going forward

---
+++ include/NamespaceDefines.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#define BEGIN_PYRAMID_NAMESPACE namespace pyramid_scheme_simulator {
+#define END_PYRAMID_NAMESPACE }
+
+#define BEGIN_PYRAMID_GL_NAMESPACE namespace pyramid_scheme_simulator { namespace gl {
+#define END_PYRAMID_GL_NAMESPACE } }
