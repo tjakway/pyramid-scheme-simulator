4a4b3402f1207964ed497b4c786833ed10f8a6bb
Thomas Jakway <tjakway@nyu.edu>
2017-12-29 11:48:03 -0800

    first iteration of TextureHandle header

---
+++ include/gl/TextureHandle.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "NamespaceDefines.hpp"
+
+#include <string>
+#include <GL/gl.h>
+
+BEGIN_PYRAMID_GL_NAMESPACE
+
+class TextureHandle
+{
+protected:
+    const GLuint handle;
+    TextureHandle(GLuint);
+
+public:
+    TextureHandle(TextureHandle&&);
+    virtual ~TextureHandle();
+
+    //TODO: if debug, check texture handle
+    GLuint get() const;
+
+    static TextureHandle loadTexture(std::string);
+};
+
+END_PYRAMID_GL_NAMESPACE
