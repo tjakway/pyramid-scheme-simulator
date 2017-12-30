9b945e1e4e8639c17faea7fd0214782ef8081730
Thomas Jakway <tjakway@nyu.edu>
2017-12-30 09:59:27 -0800

    skeleton header for opengl state management

---
+++ include/gl/GLContext.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+class GLContext
+{
+    class Window;
+
+    static const std::string vertexShaderSource,
+                 fragmentShaderSource;
+
+public:
+};
