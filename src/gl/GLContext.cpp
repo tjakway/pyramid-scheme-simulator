cb7c1882e4ab4b9560741cc0ddd218711ab43f1e
Thomas Jakway <tjakway@nyu.edu>
2017-12-30 18:24:29 -0800

    wrote initialization code for GLWindow

--- src/gl/GLContext.cpp
+++ src/gl/GLContext.cpp
@@ -3,6 +3,9 @@
 #include "NamespaceDefines.hpp"
 #include "Util/Util.hpp"
 
+#include <utility>
+#include <functional>
+
 #include <gtkmm.h>
 
 BEGIN_PYRAMID_GL_NAMESPACE
@@ -12,13 +15,28 @@ const std::string GLContext::windowTitle = "Pyramid Scheme Simulator";
 const std::string GLContext::applicationId = "pyramid_scheme_simulator"
 
 
-GLContext::GLContext()
-{
-    windowData->app = 
-        Gtk::Application::create(argc, argv,
-            windowName);
+GLContext::GLContext(
+        const Config::BackendOptions::GLBackendOptions::WindowOptions& windowOptions)
+    : glWindow(make_unique<GLWindow>(GLContext::windowTitle,
+            std::make_pair(windowOptions.width, windowOptions.height),
+            std::bind(glInit, this),
+            std::bind(glDraw, this),
+            std::bind(glCleanup, this),
+            openglRequiredMajorVersion = -1,
+            openglRequiredMinorVersion = -1))
+{}
 
-    windowData->window.set_default_size(200, 200);
+
+void GLContext::run(Glib::RefPtr<Application> app)
+{
+    glWindow->run(app, GLContext::applicationId);
+}
+
+void GLContext::run()
+{
+    const auto app = Gtk::Application::create(
+            GLContext::applicationId);
+    run(app);
 }
 
 END_PYRAMID_GL_NAMESPACE
