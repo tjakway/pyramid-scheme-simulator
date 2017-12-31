448f47e43d5b119d6bb8ff300baae15909b33724
Thomas Jakway <tjakway@nyu.edu>
2017-12-30 18:45:43 -0800

    fixed pkg config behavior--changed target_compile_definitions -> target_compile_options

--- test/include/BasicGraphSetup.hpp
+++ test/include/BasicGraphSetup.hpp
@@ -4,12 +4,9 @@
 
 #include "Config.hpp"
 #include "Util/Util.hpp"
-#include "Util/Unique.hpp"
-#include "TransactionRecords.hpp"
 #include "TransactionObjects.hpp"
 #include "PopulationGraph.hpp"
 #include "Types.hpp"
-#include "CapitalHolderClassDecls.hpp"
 #include "CapitalHolder.hpp"
 #include "PopulationGraph.hpp"
 
