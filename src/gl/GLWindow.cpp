9e6fc7988b2511d234dc39d0ba880556715a9ec7
Thomas Jakway <tjakway@nyu.edu>
2018-01-04 13:13:44 -0800

    added setOpenGLAttributes and createGLContext

--- src/gl/GLWindow.cpp
+++ src/gl/GLWindow.cpp
@@ -1,5 +1,7 @@
 #include "gl/GLWindow.hpp"
 
+#include <string>
+
 #include <SDL.h>
 #include "Util/NewExceptionType.hpp"
 
@@ -12,8 +14,12 @@ private:
     //to prevent trying to initialize it twice
     static std::atomic_bool sdlInitialized;
 
-public:
-    NEW_EXCEPTION_TYPE_WITH_BASE(SDLInitException, GLWindowInitException);
+    NEW_EXCEPTION_TYPE_WITH_BASE(SDLException, GLWindowException);
+    NEW_EXCEPTION_TYPE_WITH_BASE(SDLInitException, SDLException);
+    NEW_EXCEPTION_TYPE_WITH_BASE(SDLCreateWindowException, SDLInitException);
+    NEW_EXCEPTION_TYPE_WITH_BASE(SDLCreateGLContextException, SDLInitException);
+
+    void setOpenGLAttributes();
 
     void initSDL()
     {
@@ -28,6 +34,55 @@ public:
                 sdlInitialized.store(true);
             }
         }
+    }
+
+    SDL_Window* createWindow(
+            const std::string& title, 
+            std::pair<int, int> windowDimensions)
+    {
+        SDL_Window* win = SDL_CreateWindow(title.c_str(),
+                SDL_WINDOWPOS_CENTERED,
+                SDL_WINDOWPOS_CENTERED,
+		windowDimensions.first,
+                windowDimensions.second, 
+                SDL_WINDOW_OPENGL);
+
+        if(!win)
+        {
+            throw SDLCreateWindowException(__func__);
+        }
+
+        return win;
+    }
+
+    SDL_GLContext createGLContext(SDL_Window* win)
+    {
+        SDL_GLContext context = SDL_GL_CreateContext(win);
+
+        if(!context)
+        {
+            throw SDLCreateGLContextException(__func__);
+        }
+
+        setOpenGLAttributes();
+
+        return context;
+    }
+
+    void setOpenGLAttributes(int majorVersion, int minorVersion)
+    {
+        //TODO: do I need this?
+	SDL_GL_SetSwapInterval(1);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+        if(majorVersion >= 1 && minorVersion >= 1)
+        {
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, majorVersion);
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, minorVersion);
+        }
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     }
 };
 
