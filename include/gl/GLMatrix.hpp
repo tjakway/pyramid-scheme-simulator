c00a12c71a9632fc47bee226c6991feaa33899cb
Thomas Jakway <tjakway@nyu.edu>
2018-01-05 14:09:19 -0800

    fixup! wrote matrix setup

--- include/gl/GLMatrix.hpp
+++ include/gl/GLMatrix.hpp
@@ -9,13 +9,14 @@ BEGIN_PYRAMID_GL_NAMESPACE
 
 class GLMatrix
 {
+    GLMatrix() = delete;
 public:
     static const glm::vec3 origin;
     static const glm::mat4 identityMatrix;
 
-    glm::mat4 calculateWithModelMatrix(const glm::mat4&);
+    static glm::mat4 calculateWithModelMatrix(const glm::mat4&);
 
-    void updateMVPUniform(const glm::mat4&);
+    static void updateMVPUniform(const glm::mat4&);
 };
 
 END_PYRAMID_GL_NAMESPACE
