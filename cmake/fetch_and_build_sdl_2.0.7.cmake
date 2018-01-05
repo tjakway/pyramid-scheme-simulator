e14492195a0c83a41c633033cfaee8dca52ec03b
Thomas Jakway <tjakway@nyu.edu>
2018-01-04 22:08:19 -0800

    fixup! script to download and build the latest SDL release

---
+++ cmake/fetch_and_build_sdl_2.0.7.cmake
@@ -0,0 +1,24 @@
+function(fetch_and_build_sdl2_release)
+    set(SDL2_RELEASE SDL2-2.0.7)
+    set(SDL2_RELEASE_FILENAME ${SDL2_RELEASE}.zip)
+
+    #make the extract dir if it doesn't exist
+    set(SDL2_EXTRACT_DIR ${CMAKE_BINARY_DIR}/sdl2_extract)
+
+    if(NOT EXISTS ${SDL2_EXTRACT_DIR})
+        file(MAKE_DIRECTORY ${SDL2_EXTRACT_DIR})
+    endif()
+
+    #download the zip
+    set(SDL2_DOWNLOAD_DEST ${SDL2_EXTRACT_DIR}/${SDL2_RELEASE_FILENAME})
+
+    file(DOWNLOAD "https://www.libsdl.org/release/${SDL2_RELEASE_FILENAME}" 
+        ${SDL2_DOWNLOAD_DEST} SHOW_PROGRESS)
+
+    execute_process(COMMAND unzip ${SDL2_RELEASE_FILENAME} 
+        WORKING_DIRECTORY ${SDL2_EXTRACT_DIR}
+        OUTPUT_QUIET)
+
+    #build the archive we just extracted
+    add_subdirectory(${SDL2_EXTRACT_DIR} ${CMAKE_BINARY_DIR}/sdl2_bin)
+endfunction()
