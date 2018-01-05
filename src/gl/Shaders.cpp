32e848a64c243f0e96d1565939cfb754197bb6f5
Thomas Jakway <tjakway@nyu.edu>
2018-01-05 13:18:22 -0800

    wrote the complementary fragment shader

--- src/gl/Shaders.cpp
+++ src/gl/Shaders.cpp
@@ -10,20 +10,20 @@ const std::string GLContext::vertexShaderSource =
 R"RAW(
 #version 420
 
-uniform mat4 mvp_matrix;
+layout(location = VS_MVP_LOC) uniform mat4 mvp_matrix;
 
 layout(location = VS_MODELSPACE_LOC) in vec3 in_vertex_position;
 layout(location = VS_TEXCOORD_LOC) in vec2 in_tex_coords;
 layout(location = VS_COLOR_LOC) in vec4 in_colors;
 
-out vec2 out_tex_coords;
-out vec4 out_colors;
+out vec2 frag_tex_coords;
+out vec4 frag_colors;
 
 void main() {
   gl_Position = vec4(vertex_position, 1.0);
 
-  out_tex_coords = in_tex_coords;
-  out_colors = in_colors;
+  frag_tex_coords = in_tex_coords;
+  frag_colors = in_colors;
 }
 )RAW";
 
@@ -32,10 +32,15 @@ const std::string GLContext::fragmentShaderSource =
 R"RAW(
 #version 420
 
-out vec4 fragColour;
+layout(location = FS_TEX_LOC) uniform sampler2D tex_sampler;
+
+in vec2 frag_tex_coords;
+in vec4 frag_colors;
+
+out vec4 colour;
 
 void main() {
-  fragColour = vec4(1.0, 0.0, 0.0, 1.0);
+    color = texture(tex_sampler, frag_tex_coords) * frag_colors;
 }
 )RAW";
 
