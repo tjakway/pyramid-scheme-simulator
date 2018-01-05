27cf4b19a4ff59723f37d6c4a350cea526dc731a
Thomas Jakway <tjakway@nyu.edu>
2018-01-05 14:04:47 -0800

    wrote updateMVPUniform

--- include/gl/GLMatrix.hpp
+++ include/gl/GLMatrix.hpp
@@ -10,7 +10,12 @@ BEGIN_PYRAMID_GL_NAMESPACE
 class GLMatrix
 {
 public:
+    static const glm::vec3 origin;
+    static const glm::mat4 identityMatrix;
+
     glm::mat4 calculateWithModelMatrix(const glm::mat4&);
+
+    void updateMVPUniform(const glm::mat4&);
 };
 
 END_PYRAMID_GL_NAMESPACE
--- src/gl/GLMatrix.cpp
+++ src/gl/GLMatrix.cpp
@@ -1,4 +1,7 @@
 #include "gl/GLMatrix.hpp"
+#include "gl/ShaderDefines.hpp"
+
+#include <GL/glew.h>
 
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
@@ -7,8 +10,8 @@
 
 BEGIN_PYRAMID_GL_NAMESPACE
 
-static const glm::vec3 origin = glm::vec3(0, 0, 0);
-static const glm::mat4 identityMatrix = glm::mat4(1.0f);
+const glm::vec3 GLMatrix::origin = glm::vec3(0, 0, 0);
+const glm::mat4 GLMatrix::identityMatrix = glm::mat4(1.0f);
 
 static const glm::mat4 startingProjection = glm::ortho(-1.0, 1.0, -1.0, 1.0);
 
@@ -29,6 +32,11 @@ static const glm::mat4 calculateMVP(
 glm::mat4 GLMatrix::calculateWithModelMatrix(const glm::mat4& model)
 {
     return calculateMVP(startingProjection, startingCamera, model);
+}
+
+void GLMatrix::updateMVPUniform(const glm::mat4& matrix)
+{
+    glUniformMatrix4fv(VS_MVP_LOC, 1, GL_FALSE, &matrix[0][0]);
 }
 
 END_PYRAMID_GL_NAMESPACE
