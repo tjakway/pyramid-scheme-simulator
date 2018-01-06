ec4816c943589b115903ae01bf2a88aa7c698583
Thomas Jakway <tjakway@nyu.edu>
2018-01-06 10:03:44 -0800

    writing buffer gen code

--- include/gl/VAOHandle.hpp
+++ include/gl/VAOHandle.hpp
@@ -6,6 +6,7 @@
 
 #include "gl/GLResourceHandle.hpp"
 #include "gl/GLUtil.hpp"
+#include "gl/ShaderDefines.hpp"
 
 #include <GL/glew.h>
 
@@ -44,14 +45,31 @@ private:
     static constexpr int numVertices = 4,
                      numPositionDimensions = 3, //x, y, z
                      numTexCoordDimensions = 2, //u, v
-                     numColorDimensions = 4;    //r, g, b, a
+                     numColorDimensions = 4,    //r, g, b, a
+                     
+                     //AKA stride
+                     totalOffset = sizeof(GLfloat) *  //stride needs to be in bytes
+                                    (numPositionDimensions + 
+                                     numTexCoordDimensions + 
+                                     numColorDimensions),
+                     
+                     //the start of each dimension's data in the array
+                     positionOffset = sizeof(GLfloat) * 0,
+                     texCoordOffset = positionOffset + (sizeof(GLfloat) * numPositionDimensions),
+                     colorOffset = texCoordOffset + (sizeof(GLfloat) * numTexCoordDimensions);
+
 
     virtual std::array<float, numPositionDimensions*numVertices> getPositionData();
     virtual std::array<float, numTexCoordDimensions*numVertices> getTexCoordData();
     virtual std::array<float, numColorDimensions*numVertices> getColorData();
 
+    static constexpr int numVBOElements = 
+        (numPositionDimensions + numTexCoordDimensions + numColorDimensions) * numVertices;
 
-    static InternalVAOHandle genVAO()
+    //combine the arrays of each dimension
+    std::array<float, numVBOElements> interleaveVertexData();
+
+    InternalVAOHandle genVAO(GLenum hint = GL_STATIC_DRAW)
     {
         InternalVAOHandle handle;
 
@@ -61,8 +79,15 @@ private:
 
         glGenBuffers(1, &handle.vboId);
 
+        //get the vertex data
+        const auto vertexData = interleaveVertexData();
+
         //fill the vbo
+        glBindBuffer(GL_ARRAY_BUFFER, handle.vboId);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * numVBOElements, vertexData.data(), hint);
         
+        //indicate data offsets for each attribute
+        glVertexAttribPointer(VS_POSITION_LOC, )
     }
 
     static void errorCheck(InternalVAOHandle handle)
