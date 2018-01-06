86ce3a6e3ab97613090d17a20c357b9ecb634a64
Thomas Jakway <tjakway@nyu.edu>
2018-01-06 10:43:19 -0800

    added a header for non-specific exception types

---
+++ include/Util/GeneralExceptions.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "NamespaceDefines.hpp"
+#include "Util/NewExceptionType.hpp"
+
+BEGIN_PYRAMID_NAMESPACE
+
+//for sanity checks
+NEW_EXCEPTION_TYPE(ImplementationException);
+
+END_PYRAMID_NAMESPACE
