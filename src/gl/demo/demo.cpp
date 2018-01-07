eba5446bcc6d80bddbc75f3c941848b3fd9a5a9a
Thomas Jakway <tjakway@nyu.edu>
2018-01-06 21:39:55 -0800

    demo code is obsolete

--- src/gl/demo/demo.cpp
+++ src/gl/demo/demo.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 
 #include "Config.hpp"
-#include "gl/GLContext.hpp"
+#include "gl/GLBackend.hpp"
+
 
 using namespace pyramid_scheme_simulator;
 using namespace pyramid_scheme_simulator::gl;
+
+#include "Actors.hpp"
+#include "Util/Unique.hpp"
+#include "Types.hpp"
 
 int main(int argc, char** argv)
 {
     std::cout << "Hello, SDL2!" << std::endl;
 
-    GLContext(Config::Defaults::defaultWindowOptions).run();
+
 }
