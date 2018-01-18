56d8313e41d7a2c236521d1c24fbca307f04b8f1
Thomas Jakway <tjakway@nyu.edu>
2018-01-18 12:11:20 -0800

    moved EagerConsumer and EagerDistributor out of test code and into main source

--- test/include/mocks/EagerActors.hpp
+++ test/include/mocks/EagerActors.hpp
@@ -3,17 +3,9 @@
 #include "Actors.hpp"
 #include "Util/Unique.hpp"
 #include "Types.hpp"
+#include "ai/EagerActors.hpp"
 
 namespace pyramid_scheme_simulator {
-
-class EagerConsumer : public StaticConsumer
-{
-public:
-    EagerConsumer(Unique u, Money m)
-        : StaticConsumer(u, m, 1.0, 1.0)
-    {}
-    virtual ~EagerConsumer() {}
-};
 
 /**
  * EagerConsumer with a random UUID
@@ -25,32 +17,6 @@ public:
         : EagerConsumer(Unique::newUnique(), m)
     {}
     virtual ~EagerTestConsumer() {}
-};
-
-class EagerDistributor : public StaticDistributor
-{
-public:
-    EagerDistributor(Unique u, Money m, Inventory i)
-        : StaticDistributor(u, m, i, Config::Defaults::defaultRestockThreshold, 1.0)
-    {}
-
-    EagerDistributor(Consumer& self, std::shared_ptr<Distributor> convBy) 
-        : StaticDistributor(self, convBy)
-    {}
-
-    EagerDistributor(const EagerDistributor& other)
-        : EagerDistributor(other.id, other.getMoney(), other.getInventory())
-    {}
-
-    virtual ~EagerDistributor() {}
-
-    virtual Inventory getDesiredRestockAmount() const override {
-        return 5;
-    }
-
-    virtual Inventory getRestockThreshold() const override {
-        return 1;
-    }
 };
 
 class EagerTestDistributor : public EagerDistributor
